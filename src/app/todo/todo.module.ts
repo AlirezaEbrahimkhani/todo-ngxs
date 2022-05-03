f9b712297ab093c4d190aa2a5cc771a6235d7b65
AlirezaEbrahimkhani <alirezaebrahimkhanidev@gmail.com>
2022-05-03 20:43:12 +0430

    feat(todo): create todo-card component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoComponent } from './todo.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TodoCardComponent } from './todo-card/todo-card.component';
 
 const routes: Routes = [
   {
@@ -10,8 +14,16 @@ const routes: Routes = [
   },
 ];
 
+const Mat_Modules = [MatInputModule, MatFormFieldModule];
+
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
-  declarations: [TodoComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    ...Mat_Modules,
+  ],
+  declarations: [TodoComponent, TodoCardComponent],
 })
 export class TodoModule {}
