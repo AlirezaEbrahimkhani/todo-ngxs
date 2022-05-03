5999b759f7c12d58a4847ba6fa22c390fa2d4b65
AlirezaEbrahimkhani <alirezaebrahimkhanidev@gmail.com>
2022-05-03 20:31:12 +0430

    feat(todo): create todo component and route registering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'todo',
+  },
+  {
+    path: 'todo',
     loadChildren: () => import('./todo/todo.module').then((m) => m.TodoModule),
   },
 ];
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoComponent,
+  },
+];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [TodoComponent],
 })
 export class TodoModule {}
